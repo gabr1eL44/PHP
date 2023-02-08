c97d4987807ff06fa036b6ea60b6696b40ff4979
gabr1eL44 <gabrielinc@gmx.de>
2023-02-08 12:52:03 +0100

    Aufgabe 1-3

---
+++ Aufgabe_3.php
@@ -0,0 +1,13 @@
+<?php
+    $str1 = "wie";
+    $str2 = "dir";
+    $str3 = "es";
+    $str4 = "Danke";
+    $str5 = "Hallo";
+    $str6 = "geht";
+    $str7 = "gut";
+    $str8 = "mir";
+
+    echo "<p>".$str5.", ".$str1." ".$str6." ".$str3." ".$str2."?</p>";
+    echo "<p>".$str4.", ".$str8." ".$str6." ".$str3." ".$str7."?</p>";
+?>
